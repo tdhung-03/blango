9645decaf4c40d418ff0e729124ab5df63581d40
tdhung03 <mspotify208@gmail.com>
2023-03-29 23:21:01 +0700

    Security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -170,6 +170,13 @@ class Dev(Configuration):
         },
     }
 
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
 
 class Prod(Dev):
     DEBUG = False
