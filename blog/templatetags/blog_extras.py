d02dfecc4b35e44d32daf06856fdb4b666bcf076
tdhung03 <mspotify208@gmail.com>
2023-03-29 12:51:43 +0700

    Filters with arguments

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,16 +2,18 @@ from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 
-
 user_model = get_user_model()
 register = template.Library()
 
 
 @register.filter
-def author_details(author):
+def author_details(author, current_user=None):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
