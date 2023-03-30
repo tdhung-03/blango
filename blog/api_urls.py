05cff9b16c9f8c0459b86996742492c50cfca504
tdhung03 <mspotify208@gmail.com>
2023-03-30 21:40:05 +0700

    Optional format suffixes

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from rest_framework.urlpatterns import format_suffix_patterns
 from blog.api_views import post_list, post_detail
 
 urlpatterns = [
     path("posts/", post_list, name="api_post_list"),
-    path("posts/<int:pk>/", post_detail, name="api_post_detail"),
+    path("posts/<int:pk>", post_detail, name="api_post_detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
