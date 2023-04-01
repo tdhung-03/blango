8d69d0e913bdb58f1741cd8f275fdee1c37def8a
tdhung03 <mspotify208@gmail.com>
2023-04-01 22:16:11 +0700

    Related Fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -4,6 +4,8 @@ from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAdminUser
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 
 
 class PostList(generics.ListCreateAPIView):
@@ -16,3 +18,9 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
